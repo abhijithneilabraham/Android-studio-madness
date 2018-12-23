4c6283933042f7f23b10e838db0d02af093f25bd
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2018-12-24 00:47:00 +0530

    fragment changed to fragnment +activity

---
+++ Try/app/src/main/java/com/example/abhijithneilabraham/atry/ui/fragment1/Fragment1.java
@@ -0,0 +1,36 @@
+package com.example.abhijithneilabraham.atry.ui.fragment1;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.abhijithneilabraham.atry.R;
+
+public class Fragment1 extends Fragment {
+
+    private Fragment1ViewModel mViewModel;
+
+    public static Fragment1 newInstance() {
+        return new Fragment1();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment1, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(Fragment1ViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ Try/app/src/main/java/com/example/abhijithneilabraham/atry/ui/fragment1/Fragment1ViewModel.java
@@ -0,0 +1,7 @@
+package com.example.abhijithneilabraham.atry.ui.fragment1;
+
+import android.arch.lifecycle.ViewModel;
+
+public class Fragment1ViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
